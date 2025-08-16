afd403b76df7266c3d96af4757d2f00428e2b2e0
Sayed Haider <sayedhaider2002@gmail.com>
2025-08-16 18:03:19 +0300

    first commit

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = 'quick-quotation-v1';
+const assets = [
+  './',
+  './index.html',
+  './style.css',
+  './script.js',
+  './manifest.json',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then(response => response || fetch(e.request))
+  );
+});
